f099129a0b1c69357726b521f8576e90fbf34654
Jamshidbek_Karimov1 <jamshidbekarimov@gmail.com>
2024-09-26 09:11:36 +0500

    auth service classes added

--- src/main/java/uz/jk/bank/domain/response/UserResponse.java
+++ src/main/java/uz/jk/bank/domain/response/UserResponse.java
@@ -2,10 +2,17 @@ package uz.jk.bank.domain.response;
 
 import lombok.*;
 
+import java.time.LocalDate;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @Builder
 public class UserResponse extends BaseResponse {
+    private String fullName;
+    private String username;
+    private String password;
+    private String email;
+    private LocalDate dateOfBirth;
 }
